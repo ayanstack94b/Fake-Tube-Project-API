6fa82d693010ecbc558fab9531a9b855d5cb77b5
ayanstack94b <ayondevs.94b@gmail.com>
2025-03-15 17:13:47 +0530

    displayed all videos

--- Script/index.js
+++ Script/index.js
@@ -1,16 +1,15 @@
 
-
 function loadCategories() {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
         .then((res) => res.json())
         .then((data) => displayCategories(data.categories))
 }
 
-
-// {
-//     "category_id": "1001",
-//     "category": "Music"
-// }
+function loadVideos() {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+}
 function displayCategories(categories) {
     const categoryContainer = document.getElementById('category-container')
 
@@ -24,4 +23,63 @@ function displayCategories(categories) {
     }
 }
 
+// {
+//     "category_id": "1003",
+//     "video_id": "aaai",
+//     "thumbnail": "https://i.ibb.co/kc8CCFs/30-rock.png",
+//     "title": "30 Rock",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/YZN9rQZ/tina.jpg",
+//             "profile_name": "Tina Fey",
+//             "verified": false
+//         }
+//     ],
+//     "others": {
+//         "views": "4.5K",
+//         "posted_date": "14800"
+//     },
+//     "description": "'30 Rock,' led by Tina Fey, is a comedy series that has garnered 4.5K views. The show is a witty and humorous take on the behind-the-scenes antics of a fictional live comedy show. With its sharp writing and unforgettable characters, '30 Rock' is perfect for fans of smart, satirical humor and engaging storylines."
+// }
+
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videoContainer')
+    videos.forEach(video => {
+        console.log(video)
+        const videoCard = document.createElement('div')
+        videoCard.innerHTML = `
+        
+       <div class="card bg-base-100 ">
+            <figure class="relative">
+                <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" />
+                <span class="absolute bottom-2 right-2 text-white bg-black px-2 rounded text-sm">3hrs 56 min ago</span>
+            </figure>
+            <div class=" flex gap-3 px-0 py-5">
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                            <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                    </div>
+                </div>
+                <div class="intro">
+                    <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                    <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}
+
+                        <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png" alt=""
+                            srcset="">
+                    </p>
+                    <p class="text-sm text-gray-400">${video.others.views}</p>
+                </div>
+
+            </div>
+        </div>
+
+        `
+        videoContainer.append(videoCard)
+    })
+}
+
 loadCategories()
+loadVideos()
